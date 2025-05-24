e367198664cc68bc53dfd100370ef5d3b4af16c8
IvanClarion <ivandaleclarion@gmail.com>
2025-05-24 11:13:56 +0800

    visual bug (landing page) fixed

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -10,16 +10,22 @@ function Offer() {
         whileInView={{y:0,opacity:1}}
         transition={{duration:1, delay:0.2}}
         >
-            <div className='flex lg:flex-row flex-col w-full justify-between items-center lg:gap-0 gap-2'>
+            <div className='flex lg:flex-row flex-col justify-between items-center '>
              <article className='flex flex-col'>
                 <label className='lg:text-3xl text-xl  font-semibold'>Connect with me</label>
-                <p className='lg:text-base-xl font-semibold'>Do you want to be my client? Work with me!</p>
+                <p className='lg:text-base-xl font-semibold mb-2'>Do you want to be my client? Work with me!</p>
+                <button>Contact Me</button>
             </article>
+           </div>
+           
             <form>
+                <label className='text-lg font-semibold'>Or let me contact you</label>
+                <div className='flex items-stretch justify-end gap-3'>
                 <input type="text" placeholder='Email' required className='offer-input'/>
                 <button className='rounded-md'>Submit</button>
+                </div>
             </form>
-            </div>
+            
         </motion.div>
     </section>
     </>
