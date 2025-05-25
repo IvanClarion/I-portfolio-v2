2e0005d8548e8e66c12abaf0e781c33553a4ba99
IvanClarion <ivandaleclarion@gmail.com>
2025-05-25 19:37:06 +0800

    widgets added

--- src/Page/ProjectsPage.jsx
+++ src/Page/ProjectsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProjectConatiner from '../components/Projects/ProjectConatiner'
+import ProjectsWidgets from '../components/Projects/ProjectWidgets'
 function ProjectsPage() {
   return (
     <>
     <section className='lg:mx-20 lg:my-5 mx-2 my-2'>
     <div>
+    <ProjectsWidgets/>
     <ProjectConatiner/>
     </div>
     </section>
---
+++ src/components/Projects/ProjectWidgets.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Folder } from 'lucide-react';
+function ProjectWidgets() {
+  return (
+    <>
+        <section className='widgets-section'>
+            <div className='widgets-container'>
+                <article>
+                    <Folder className='text-white lg:size-8 size-6'/>
+                    <header>Total Projects</header>
+                </article>
+                <span className='widgets-circular-bar'>
+                    <label>6</label>
+                </span>
+            </div>
+            <div className='widgets-container'>
+                <article>
+                    <Folder className='text-white lg:size-8 size-6'/>
+                    <header> Work Availability</header>
+                </article>
+                <span className='widgets-circular-bar-v2'>
+                    <label>Contract</label>
+                </span>
+            </div>
+        </section> 
+    </>
+  )
+}
+
+export default ProjectWidgets
